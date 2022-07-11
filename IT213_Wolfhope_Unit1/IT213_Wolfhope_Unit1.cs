db195907b82962c3768c29803b8953d2cc3ddfea
Chuck Wolfhope <chuckwolfhope@gmail.com>
2022-07-11 17:38:50 -0400

    FINAL Final Update

--- IT213_Wolfhope_Unit1/IT213_Wolfhope_Unit1.cs
+++ IT213_Wolfhope_Unit1/IT213_Wolfhope_Unit1.cs
@@ -8,18 +8,25 @@ namespace IT213_Wolfhope_Unit1
 {
     internal class IT213_Wolfhope_Unit1
     {
+        // IT213 Unit 1 Assignment - Software Basics
+        // Written by Chuck Wolfhope
         static void Main(string[] args)
         {
-            // Creates variables and stores the values
+            // Create the variables and store the values
+            // String Variables
             String name = "John Smith";
             String address = "101 N. Main Street";
             String city = "AnyTown";
             String state = "TX";
             String zip = "11111";
             String unitsTaken = "19";
+
+            // Numeric Variables
             const double pricePerUnit = 100.50;
             const int twentyUnitHourDisc = 150;
             int intUnitsTaken = int.Parse(unitsTaken);              // Convert the string unitsTaken into the int variable intUnitsTaken
+
+            // Perform calculations on the number variables
             intUnitsTaken++;                                        // Add 1 to intUnitsTaken using post increment operator
             double tuition = pricePerUnit * intUnitsTaken;          // calculates the tuition - multiply units taken by the price per unit
             double afterDiscount = tuition - twentyUnitHourDisc;    // calculates the tuition after the discount - subtract the unit discount from the tuition
